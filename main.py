26ba9ba542885be842ce1f81b7ec2c711ff03bdf
Lucio Moraes <luciomoraes@gmail.com>
2025-05-10 22:24:48 +0200

    * ability to new:  X,Y, and Direction - new loop

--- main.py
+++ main.py
@@ -4,16 +4,31 @@ def main():
     print("Enter room dimensions (width depth):")
     width, depth = map(int, input().split())
 
-    print("Enter starting position (x y direction):")
-    x, y, dir = input().split()
-    x, y = int(x), int(y)
+    while True:
+        # Clean start each loop
+        x = y = 0
+        dir = ''
+        commands = ''
 
-    print("Enter commands:")
-    commands = input().strip()
+        print("\nEnter starting position (x y direction):")
+        x, y, dir = input().split()
+        x, y = int(x), int(y)
 
-    robot = Robot(width, depth, x, y, dir)
-    robot.execute_commands(commands)
-    print(robot.report())
+        print("Enter commands:")
+        commands = input().strip()
+
+        try:
+            robot = Robot(width, depth, x, y, dir)
+            robot.execute_commands(commands)
+            print(robot.report())
+        except ValueError as e:
+            print(e)
+
+        print("\nDo you want to run another command? (yes/no):")
+        answer = input().strip().lower()
+        if answer != 'yes':
+            print("Exiting the program.")
+            break
 
 if __name__ == "__main__":
     main()
